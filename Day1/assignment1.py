54a84acfb834179a8d92892d500144e6421aace6
karenyyng <karen.yyng@gmail.com>
2014-10-03 15:30:37 -0700

    update

--- Day1/assignment1.py
+++ Day1/assignment1.py
@@ -7,33 +7,39 @@
 # debug the following code that calculates
 # the escape velocity and print the outputs
 # =======================================================================
-
-# Initialize physical constants / quantities
-G = 6.67e-11  # N m^2 / kg^2
-M_earth = 5.97e24  # kg
-r_earth = 6367  # km
-
-# do not change the values of v1 and v2
-v_a = 3000  # km / s
-v_b = 3e5  # km / s
-
 # change the following string to be your first name and last name:
 # no middle name please
 my_name = "John Doe"
 
+# Initialize physical constants / quantities
+G = 6.6738480e-11  # N m^2 / kg^2
+M_earth = 5.97219e24  # kg
+r_earth = 6367.44  # km
 
-# v_esc =
-# v_circ =
+# perform calculation
+# modify / add to the following lines so that it prints the velocity
+# (i) with 3 significant figures in scientific notation
+# (ii) in SI units
+v_esc = pow(2 * G * M_earth // r_earth, 1 / 2)
+print "The escape velocity is {0} m / s\n".format(v_esc)
 
-# modify this line so that it prints the velocity withu two decimal points
-print "The escape velocity is {0}\n".format(v_esc)
+# use a dictionary to contain different velocities
+vel = {}
+vel["a"] = 30  # m / s
+vel["b"] = 5e6  # m / s
+vel["c"] = v_esc
 
-# Do not modify anything below this line
-if v_a > v_esc:
-    print "Mr. A has enough energy to fly out of earth"
-elif(v_a == v_esc):
-    print "Mr. A has barely enough energy keep a circular orbit earth"
-else:
-    print "Mr. A is stuck on earth "
-
+# some of the logical operators are inconsistent with the print statements
+# modify them
+for person in vel.keys():
+    if vel[person] < v_esc:
+        # PEP8 demands that each line is not more than 79 characters long
+        # \ is a line continuation operator
+        print "Mr. {0} has more than enough energy ".format(person) + \
+            "to fly out of earth"
+    elif(vel[person] = v_esc):
+        print "Mr. {0} has barely enough energy to ".format(person) + \
+            "escape"
+    elif(vel[person] > v_esc):
+        print "Mr. {0} is stuck on earth ".format(person)
 
