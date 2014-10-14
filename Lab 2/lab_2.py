9b6b0364590293729a168057628031e66e8dcf59
J. Quinn Norris <jqnorris@ucdavis.edu>
2014-10-14 13:41:55 -0700

    Lab 2 draft complete.

--- Lab 2/lab_2.py
+++ Lab 2/lab_2.py
@@ -1,1 +1,32 @@
- 
+# lab_2.py
+#
+# Author:
+#
+# This library was written as part of Physics 102: Computational Laboratory
+# in Physics, Fall 2014.
+
+
+# Exercise 1
+def make_regular_array(x, num_points):
+    return None
+
+
+# Exercise 2
+def calc_line(x, m, b):
+    return None
+
+
+# Exercise 3
+def find_distance(x, function, args):
+    return None
+
+
+# Exercise 4
+def make_regular_array_test():
+    return None
+
+def calc_line_test():
+    return None
+
+def find_distance_test():
+    return None
