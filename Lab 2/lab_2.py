c2437f1deb2eb022ff6d7e250fd7aa790d1efb03
karenyyng <karen.yyng@gmail.com>
2014-10-14 14:43:24 -0700

    added clearer instructions and docstrings

--- Lab 2/lab_2.py
+++ Lab 2/lab_2.py
@@ -1,32 +1,78 @@
 # lab_2.py
 #
 # Author:
+# Student ID:
 #
 # This library was written as part of Physics 102: Computational Laboratory
 # in Physics, Fall 2014.
 
+# import required libraries
+
 
 # Exercise 1
 def make_regular_array(x, num_points):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    x = numpy array
+    num_points = integer - optional argument, default value = 100
+
+    Returns
+    ======
+    1D numpy array with evenly spaced number of points, and
+    same support as x
+    """
+    # start your code here
     return None
 
 
 # Exercise 2
 def calc_line(x, m, b):
+    """calculate values predicted by our straight line model
+    Arguments
+    ==========
+    x = numpy array
+    m = float - slope of the straight line model
+    b = float - intercept
+
+    Returns
+    =======
+    numpy array with the same size as x
+    """
+    # start your code here
     return None
 
 
 # Exercise 3
 def find_distance(x, function, args):
+    """compute distance between x and predicted values by function
+    Arguments
+    ==========
+    x = numpy array
+    function = a python function that computes our straight line model
+    args = list of variables that are the arguments for function
+
+    Returns
+    ======
+    numpy array that denotes the distance between data x and model
+    """
+    # start your code here
     return None
 
 
+# You should write your own doc string to denote
+# 1) the name of the arguments
+# 2) the types of the arguments
+# 3) similar info for the returned variables if there is any
 # Exercise 4
 def make_regular_array_test():
+    # start your code here with documentation
     return None
 
 def calc_line_test():
+    # start your code here with documentation
     return None
 
 def find_distance_test():
+    # start your code here with documentation
     return None
--- Lab 2/sample_library.py
+++ Lab 2/sample_library.py
@@ -1,12 +1,13 @@
-# A sample library for Physics 102 
- 
+# A sample library for Physics 102
 pi = 3.1415962
+
 
 def hello_world():
     print "Hello World!"
     return
 
-name=''
+name = ''
+
 
 def my_name():
     if len(name) > 0:
@@ -15,4 +16,4 @@ def my_name():
         print "No Name"
     return
 
-      
+
