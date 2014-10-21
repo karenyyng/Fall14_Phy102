cede090d291531e3d0dbd9c605169e97437b87d4
J. Quinn Norris <jqnorris@ucdavis.edu>
2014-10-21 10:20:54 -0700

    Added lab_3.py library with function headers.

---
+++ Lab 3/lab_3.py
@@ -0,0 +1,123 @@
+# lab_2.py
+#
+# Author:
+# Student ID:
+#
+# This library was written as part of Physics 102: Computational Laboratory
+# in Physics, Fall 2014.
+
+# import required libraries
+
+
+# Exercise 1
+def expectation_S_x(up, down):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    up = up component of spinor
+    down = down component of spinor 
+    
+    Note: up and down do not have to be normalized
+
+    Returns
+    ======
+    float = expectation value of S_x
+    """
+    # start your code here
+    return None
+
+def expectation_S_x(up, down):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    up = up component of spinor
+    down = down component of spinor 
+    
+    Note: up and down do not have to be normalized
+
+    Returns
+    ======
+    float = expectation value of S_x
+    """
+    # start your code here
+    return None
+
+def expectation_S_y(up, down):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    up = up component of spinor
+    down = down component of spinor 
+    
+    Note: up and down do not have to be normalized
+
+    Returns
+    ======
+    float = expectation value of S_x
+    """
+    # start your code here
+    return None
+
+def expectation_S_z(up, down):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    up = up component of spinor
+    down = down component of spinor 
+    
+    Note: up and down do not have to be normalized
+
+    Returns
+    ======
+    float = expectation value of S_x
+    """
+    # start your code here
+    return None
+
+def expectation_S_sq(up, down):
+    """create numpy array with evenly spaced points
+    Arguments
+    =========
+    up = up component of spinor
+    down = down component of spinor 
+    
+    Note: up and down do not have to be normalized
+
+    Returns
+    ======
+    float = expectation value of S_x
+    """
+    # start your code here
+    return None
+
+# Exercise 2
+def is_chaotic(J):
+    """Determines whether the Jacobian matrix is chaotic
+    Arguments
+    ==========
+    J = string, list of lists, array of arrays, or matrix
+
+    Returns
+    =======
+    bool = True if chaotic, False otherwise
+    """
+    # start your code here
+    return None
+
+
+# Exercise 3
+def make_plot():
+    """Makes a plot similar to sample_plot.png and saves it in the
+    current directory as "exercise_3.pdf".
+    Arguments
+    ==========
+    None
+
+    Returns
+    ======
+    None
+    """
+    # start your code here
+    return None
+
+
