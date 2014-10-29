c0f0f0f16499f7c4c39698887c7a60e2a4264a86
J. Quinn Norris <jqnorris@ucdavis.edu>
2014-10-29 13:26:45 -0700

    Lab 4 added

---
+++ Lab 4/lab_4.py
@@ -0,0 +1,44 @@
+# lab_4.py
+#
+# Author:
+# Student ID:
+#
+# This library was written as part of Physics 102: Computational Laboratory
+# in Physics, Fall 2014.
+
+# import required libraries
+
+
+# Exercise 1
+def stock_monthly_returns():
+    """create a plot of the monthly returns for a stock
+    Arguments
+    =========
+    None
+    
+    Prompts the user for a stock ticker
+    
+    Returns
+    ======
+    None
+    
+    Saves plot to 'TICKER_monthly_returns.pdf'
+    """
+    # start your code here
+    return None
+
+def cc_decode(file_name):
+    """Decodes a text file which has been encoded using a Ceasar Cipher
+    Arguments
+    =========
+    file_name: string containing the name of the file to decode 
+
+    Returns
+    ======
+    None
+    
+    Writes decoded text to 'file_name_decoded.txt'
+    """
+    # start your code here
+    return None
+
