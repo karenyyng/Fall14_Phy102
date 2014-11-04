5c17756d08e257653482f17742b23317e58a13b2
J. Quinn Norris <jqnorris@ucdavis.edu>
2014-11-04 13:56:47 -0800

    Lab 5 first draft

---
+++ Lab 5/lab_5.py
@@ -0,0 +1,67 @@
+# lab_4.py
+#
+# Author:
+# Student ID:
+#
+# This library was written as part of Physics 102: Computational Laboratory
+# in Physics, Fall 2014.
+
+# import required libraries
+
+
+# Exercise 1
+def calc_stats(ticker):
+    """calculates basic statistical quantites for the daily returns
+    of the stock with ticker
+    
+    Arguments
+    =========
+    ticker: string containg the ticker 
+    
+    Returns
+    ======
+    None
+    
+    Saves statistical quantities to 'TICKER_daily_returns_stats.txt'
+    """
+    # start your code here
+    return None
+
+# Exercise 2
+def gaussian(x, mu, sigma)
+    """A python representation of a gaussian distribution with mean mu
+    and standard deviation sigma
+    
+    Arguments
+    =========
+    x: float, independant variable for function
+    mu: float giving the mean of the gaussian
+    sigma: float giving the standard deviation of the gaussian
+    
+    Returns
+    ======
+    float: the value of the gaussian distribution at x
+    
+    Saves statistical quantities to 'TICKER_daily_returns_stats.txt'
+    """
+    # start your code here
+    return None
+
+# Exercise 3
+def best_fit_gaussian(ticker):
+    """determines the best fit gaussian for the distribution of daily
+    returns for stock with ticker and makes a plot of the distribution
+    and the best-fit gaussian
+    
+    =========
+    ticker: string for the stock ticker
+
+    Returns
+    ======
+    None
+    
+    Writes decoded text to 'file_name_decoded.txt'
+    """
+    # start your code here
+    return None
+
